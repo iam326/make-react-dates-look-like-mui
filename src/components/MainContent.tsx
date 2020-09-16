5d20d8507dd9f20132d14f7f5043dcbbf8ccf14c
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-09-16 13:54:58 +0900

    modify main content

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
 import MuiDatePicker from './MuiDatePicker';
-import ReactDatesDateRangePicker from './ReactDatesDateRangePicker';
+// import ReactDatesDateRangePicker from './ReactDatesDateRangePicker';
+import CustomReactDatesDateRangePicker from './CustomReactDatesDateRangePicker';
 
 const MainContent: React.FC = () => {
   return (
     <>
       <div>
+        <h3>Material UI - DatePicker (inline mode)</h3>
         <MuiDatePicker />
       </div>
-      <div>
+      {/* <div>
+        <h3>react-dates - DateRangePicker</h3>
         <ReactDatesDateRangePicker />
+      </div> */}
+      <div style={{ marginTop: '20px' }}>
+        <h3>Custom react-dates DateRangePicker</h3>
+        <CustomReactDatesDateRangePicker />
       </div>
     </>
   );
